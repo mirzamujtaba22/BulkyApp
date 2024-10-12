f4346c0613f3e63f9010e1098627a18b538b64e6
mirzamujtaba22 <mirzamujtaba22@gmail.com>
2024-10-12 23:36:52 +0500

    Add project files.

---
+++ BulkyApplication/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+ 
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> getallcategories = _db.Categories.ToList();
+            return View(getallcategories);
+        }
+    }
+}
